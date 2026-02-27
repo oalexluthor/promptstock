fcdb3b076ec412ec3f051d01d20c5a1c6be4a74c
alex.paula <alex.paula@agetec.campogrande.ms.gov.br>
2026-02-27 09:46:13 -0400

    automatizando o sidebar Path

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,4 +1,46 @@
 import { defineConfig } from 'vitepress'
+import fs from 'fs'
+import path from 'path'
+
+
+
+function getDynamicSidebar() {
+  const docsPath = path.resolve(__dirname, '../')
+  const sidebar = []
+
+  // Lê as subpastas (ex: code_reviewer, gerencia, sac)
+  const categories = fs.readdirSync(docsPath).filter(f => 
+    fs.statSync(path.join(docsPath, f)).isDirectory() && !f.startsWith('.')
+  )
+
+  categories.forEach(cat => {
+    const catPath = path.join(docsPath, cat)
+    const files = fs.readdirSync(catPath).filter(f => f.endsWith('.md'))
+
+    sidebar.push({
+      text: cat.replace(/_/g, ' ').toUpperCase(), // Ex: CODE REVIEWER
+      items: files.map(file => ({
+        text: file.replace('.md', '').replace(/-/g, ' ').toUpperCase(),
+        link: `/${cat}/${file.replace('.md', '')}` // Link exato para o arquivo
+      }))
+    })
+
+
+    // teste de duplicação da sidebar
+        sidebar.push({
+      text: cat.replace(/_/g, ' ').toUpperCase(), // Ex: CODE REVIEWER
+      items: files.map(file => ({
+        text: file.replace('.md', '').replace(/-/g, ' ').toUpperCase(),
+        link: `/${cat}/${file.replace('.md', '')}` // Link exato para o arquivo
+      }))
+    })
+    
+  })
+
+  return sidebar
+}
+
+
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -11,16 +53,19 @@ export default defineConfig({
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
-          { text: 'Code Reviewer', link: '/code_reviewer/agent-code-reviewer' }
-        ]
-      }
-    ],
+    sidebar: getDynamicSidebar(),
+
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' },
+    //       { text: 'Code Reviewer', link: '/code_reviewer/agent-code-reviewer' }
+    //     ]
+    //   }
+    // ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
